4c3ff10f170aacafada554e50b38b1fde93f33cb
Matthew Huang <matthew_hs@outlook.com>
2019-03-11 21:54:16 +0800

    for test

---
+++ src/test/java/com/ustc/swagger2apimanagementsystemhuawei/Swagger2ApimanagementsystemHuaweiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ustc.swagger2apimanagementsystemhuawei;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Swagger2ApimanagementsystemHuaweiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
